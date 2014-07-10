5108600be31c6a5612c6c42d64fe8acf7c019f23
--unset-all <--unset-all>
2014-07-10 11:10:08 +0700

    Fix spelling

--- src/views/partials/modal.blade.php
+++ src/views/partials/modal.blade.php
@@ -7,7 +7,7 @@
       {{ Form::open(['method' => 'DELETE', 'route' => ["admin.$name.destroy", $data->id]])}}
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title">Hapus Data</h4>
+        <h4 class="modal-title">Delete Data</h4>
       </div>
       <div class="modal-body">
         <p>
