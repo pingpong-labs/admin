f0cc032082bc80a06c11a101c6b2bcbcb9adcdf9
--unset-all <--unset-all>
2014-07-21 15:25:28 +0700

    Fixed trusty bugs when not authenticated

--- src/Pingpong/Admin/permissions.php
+++ src/Pingpong/Admin/permissions.php
@@ -1,20 +1,23 @@
 <?php
 
-Trusty::setView('admin::403');
-
-try
+if(Auth::check())
 {
-	Trusty::registerPermissions();
-}
-catch(PDOException $e)
-{
-	
-}
+	Trusty::setView('admin::403');
 
-Trusty::when(['admin/users', 'admin/users/*'], 'manage_users');
-Trusty::when(['admin/pages', 'admin/pages/*'], 'manage_pages');
-Trusty::when(['admin/articles', 'admin/articles/*'], 'manage_articles');
-Trusty::when(['admin/categories', 'admin/categories/*'], 'manage_categories');
-Trusty::when(['admin/permissions', 'admin/permissions/*'], 'manage_permissions');
-Trusty::when(['admin/roles', 'admin/roles/*'], 'manage_roles');
-Trusty::when('admin/settings', 'manage_settings');
+	try
+	{
+		Trusty::registerPermissions();
+	}
+	catch(PDOException $e)
+	{
+		
+	}
+
+	Trusty::when(['admin/users', 'admin/users/*'], 'manage_users');
+	Trusty::when(['admin/pages', 'admin/pages/*'], 'manage_pages');
+	Trusty::when(['admin/articles', 'admin/articles/*'], 'manage_articles');
+	Trusty::when(['admin/categories', 'admin/categories/*'], 'manage_categories');
+	Trusty::when(['admin/permissions', 'admin/permissions/*'], 'manage_permissions');
+	Trusty::when(['admin/roles', 'admin/roles/*'], 'manage_roles');
+	Trusty::when('admin/settings', 'manage_settings');
+}
