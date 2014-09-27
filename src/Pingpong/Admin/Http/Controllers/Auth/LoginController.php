e9388cf2fb1a164752919a03e6f7953981803987
gravitano <gue.warsono@gmail.com>
2014-09-27 20:55:41 +0700

    Validate the login form first

--- src/Pingpong/Admin/Http/Controllers/Auth/LoginController.php
+++ src/Pingpong/Admin/Http/Controllers/Auth/LoginController.php
@@ -3,16 +3,38 @@
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
+use Pingpong\Admin\Validation\Auth\Login;
 
 class LoginController extends Controller {
 
+    /**
+     * @var Login
+     */
+    protected $validator;
+
+    /**
+     * @param Login $validator
+     */
+    public function __construct(Login $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getIndex()
     {
         return aview('auth.login');
     }
 
+    /**
+     * @return mixed
+     */
     public function postIndex()
     {
+        $this->validator->validate();
+
         if (Auth::attempt(Input::only('username', 'password'), Input::has('remember')))
         {
             return app('redirect')->route('admin.home')->withSuccess('Login success');
