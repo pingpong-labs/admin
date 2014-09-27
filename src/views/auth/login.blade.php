41c630dae596f75b5d274d14941b0d086588e283
gravitano <gue.warsono@gmail.com>
2014-09-27 21:12:45 +0700

    Show validation error

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -18,10 +18,12 @@
             <div class="form-group">
                 {{ Form::label('username', 'Username:') }}
                 {{ Form::text('username', null, ['class' => 'form-control']) }}
+                {{ error_for('username', $errors) }}
             </div>
             <div class="form-group">
                 {{ Form::label('password', 'Password:') }}
                 {{ Form::password('password', ['class' => 'form-control']) }}
+                {{ error_for('password', $errors) }}
             </div>
             <div class="form-group">
                 {{ Form::submit('Login', ['class' => 'btn btn-block btn-primary']) }}
