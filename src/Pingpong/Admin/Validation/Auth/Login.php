5431f89ec5a16d7a7f82d7dd420ec9ecfba7a9fd
gravitano <gue.warsono@gmail.com>
2014-09-27 20:49:58 +0700

    Added login validator

---
+++ src/Pingpong/Admin/Validation/Auth/Login.php
@@ -0,0 +1,20 @@
+<?php namespace Pingpong\Admin\Validation\Auth;
+
+use Pingpong\Validator\Validator;
+
+class Login extends Validator {
+
+    /**
+     * The validation rules.
+     * 
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+    }
+
+}
