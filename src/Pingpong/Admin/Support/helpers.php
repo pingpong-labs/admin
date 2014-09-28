c515fc03034b5411bd1e2e6ffe12951a6572329c
gravitano <gue.warsono@gmail.com>
2014-09-28 12:09:49 +0700

    Added new helpers with some clean up

--- src/Pingpong/Admin/Support/helpers.php
+++ src/Pingpong/Admin/Support/helpers.php
@@ -1,25 +1,44 @@
 <?php
 
 use Pingpong\Admin\Menus\Menu;
+use Illuminate\Support\MessageBag;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\HTML;
 
-function option($key, $default = null)
+if ( ! function_exists('option'))
 {
-    try
+    /**
+     * Get option from database.
+     *
+     * @param $key
+     * @param null $default
+     * @return null
+     */
+    function option($key, $default = null)
     {
-        $option = Option::findByKey($key)->first();
+        try
+        {
+            $option = Option::findByKey($key)->first();
 
-        return ! empty($option) ? $option->value : $default;
-    }
-    catch (PDOException $e)
-    {
-        return $default;
+            return ! empty($option) ? $option->value : $default;
+        }
+        catch (PDOException $e)
+        {
+            return $default;
+        }
     }
 }
 
 if ( ! function_exists('style'))
 {
+    /**
+     * Generate stylesheet tag from admin's package.
+     *
+     * @param $url
+     * @param array $attributes
+     * @param bool $secure
+     * @return mixed
+     */
     function style($url, $attributes = array(), $secure = false)
     {
         return HTML::style('packages/pingpong/admin/' . $url, $attributes, $secure);
@@ -28,6 +47,14 @@ if ( ! function_exists('style'))
 
 if ( ! function_exists('script'))
 {
+    /**
+     * Generate script tag from admin's package.
+     *
+     * @param $url
+     * @param array $attributes
+     * @param bool $secure
+     * @return mixed
+     */
     function script($url, $attributes = array(), $secure = false)
     {
         return HTML::script('packages/pingpong/admin/' . $url, $attributes, $secure);
@@ -36,6 +63,14 @@ if ( ! function_exists('script'))
 
 if ( ! function_exists('aview'))
 {
+    /**
+     * Return a view from admin's package.
+     *
+     * @param $view
+     * @param array $data
+     * @param array $mergeData
+     * @return mixed
+     */
     function aview($view, array $data = [], array $mergeData = [])
     {
         return View::make('admin::' . $view, $data, $mergeData);
@@ -44,8 +79,46 @@ if ( ! function_exists('aview'))
 
 if ( ! function_exists('menu'))
 {
+    /**
+     * Render menu.
+     *
+     * @param $name
+     * @param null $presenter
+     * @return null|string
+     */
     function menu($name, $presenter = null)
     {
         return Menu::render($name, $presenter);
+    }
+}
+
+if ( ! function_exists('r'))
+{
+    /**
+     * Generate a url from the registered route.
+     *
+     * @param $route
+     * @param array $parameters
+     * @param string $prefix
+     * @return string
+     */
+    function r($route, $parameters = array(), $prefix = 'admin')
+    {
+        return route("{$prefix}.{$route}", $parameters);
+    }
+}
+
+if ( ! function_exists('error_for'))
+{
+    /**
+     * Get error message for the specified field and format it to "text-danger" style.
+     *
+     * @param $field
+     * @param MessageBag $errors
+     * @return mixed
+     */
+    function error_for($field, MessageBag $errors)
+    {
+        return $errors->first($field, '<div class="text-danger">:message</div>');
     }
 }
