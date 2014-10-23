f14a2aab6eb72963a039bb6c175edfc4488e3073
gravitano <gue.warsono@gmail.com>
2014-10-23 16:07:46 +0700

    Clean up

--- src/Pingpong/Admin/Console/AdminMigrationCommand.php
+++ src/Pingpong/Admin/Console/AdminMigrationCommand.php
@@ -21,16 +21,6 @@ class AdminMigrationCommand extends Command {
 	protected $description = 'Copy the migration file to the application.';
 
 	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
 	 * Execute the console command.
 	 *
 	 * @return mixed
