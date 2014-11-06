39d913e0fe329e1c1868e5a4ef29641b1aeae263
gravitano <gue.warsono@gmail.com>
2014-11-06 17:29:11 +0700

    Fix #13

--- src/Pingpong/Admin/Providers/SupportServiceProvider.php
+++ src/Pingpong/Admin/Providers/SupportServiceProvider.php
@@ -7,12 +7,11 @@ class SupportServiceProvider extends ServiceProvider {
 	public function boot()
 	{
         require __DIR__ . '/../permissions.php';
-        require __DIR__ . '/../routes.php';
         require __DIR__ . '/../filters.php';
         require __DIR__ . '/../composers.php';
         require __DIR__ . '/../helpers.php';
-        require __DIR__ . '/../menus.php';
         require __DIR__ . '/../observers.php';
+        require __DIR__ . '/../routes.php';
 	}
 
 	public function register()
--- src/Pingpong/Admin/routes.php
+++ src/Pingpong/Admin/routes.php
@@ -2,12 +2,12 @@
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Pingpong\Admin\Controllers'], function ()
 {
-    Route::group(['before' => Config::get('admin::filter.guest')], function ()
+    Route::group(['before' => 'admin.guest'], function ()
     {
         Route::resource('login', 'LoginController', ['only' => ['index', 'store']]);
     });
 
-    Route::group(['before' => Config::get('admin::filter.auth')], function ()
+    Route::group(['before' => 'admin.auth'], function ()
     {
         Route::get('/', ['as' => 'admin.home', 'uses' => 'SiteController@index']);
         Route::get('/logout', ['as' => 'admin.logout', 'uses' => 'SiteController@logout']);
