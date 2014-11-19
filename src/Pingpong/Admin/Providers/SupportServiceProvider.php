19ea7b243078ef7b55c28e8c7693877dc3f9f069
gravitano <gravitano>
2014-11-19 11:18:17 +0700

    Automatic register intervention/image package #14

--- src/Pingpong/Admin/Providers/SupportServiceProvider.php
+++ src/Pingpong/Admin/Providers/SupportServiceProvider.php
@@ -1,9 +1,33 @@
 <?php namespace Pingpong\Admin\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
 
 class SupportServiceProvider extends ServiceProvider {
 
+	/**
+	 * The class aliases array.
+	 * 
+	 * @var array
+	 */
+	protected $aliases = [
+		'Image' => 'Intervention\Image\Facades\Image'
+	];
+
+	/**
+	 * The service provider classes array.
+	 * 
+	 * @var array
+	 */
+	protected $providers = [
+		'Intervention\Image\ImageServiceProvider',
+	];
+
+	/**
+	 * Boot the service provider.
+	 * 
+	 * @return void
+	 */
 	public function boot()
 	{
         require __DIR__ . '/../permissions.php';
@@ -15,9 +39,38 @@ class SupportServiceProvider extends ServiceProvider {
         require __DIR__ . '/../routes.php';
 	}
 
+	/**
+	 * Register the service provider.
+	 * 
+	 * @return void
+	 */
 	public function register()
 	{
-		//	
+		$this->registerProviders();
+		$this->registerAliases();
+	}
+
+	/**
+	 * Register service providers.
+	 * 
+	 * @return void
+	 */
+	public function registerProviders()
+	{
+		foreach ($this->providers as $provider)
+		{
+			$this->app->register($provider);
+		}
+	}
+
+	/**
+	 * Register class aliases.
+	 * 
+	 * @return void
+	 */
+	public function registerAliases()
+	{
+		AliasLoader::getInstance($this->aliases)->register();
 	}
 
 }
