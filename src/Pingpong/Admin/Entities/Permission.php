67676220a0be81c75699075dec4d00bd309acf9e
gravitano <gravitano>
2014-11-19 21:32:53 +0700

    Clean up Permission model

--- src/Pingpong/Admin/Entities/Permission.php
+++ src/Pingpong/Admin/Entities/Permission.php
@@ -2,28 +2,4 @@
 
 use Pingpong\Trusty\Entities\Permission as BasePermission;
 
-class Permission extends BasePermission {
-
-    /**
-     * @var array
-     */
-    protected $rules = array(
-        'name' => 'required',
-    );
-
-    /**
-     * @return array
-     */
-    public function getRules()
-    {
-        return $this->rules;
-    }
-
-    /**
-     * @return array
-     */
-    public function getUpdateRules()
-    {
-        return $this->rules;
-    }
-}
+class Permission extends BasePermission {}
