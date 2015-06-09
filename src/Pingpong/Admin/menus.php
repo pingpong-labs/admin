9407f9812fa51e66716a8286dc14a24a71426533
Gravitano <gravitano16@gmail.com>
2015-06-09 11:36:10 +0700

    added menu ordering

--- src/Pingpong/Admin/menus.php
+++ src/Pingpong/Admin/menus.php
@@ -1,23 +1,24 @@
 <?php
 
 Menu::create('admin-menu', function ($menu) {
+    $menu->enableOrdering();
     $menu->setPresenter('Pingpong\Admin\Presenters\SidebarMenuPresenter');
-    $menu->route('admin.home', 'Dashboard', [], ['icon' => 'fa fa-dashboard']);
+    $menu->route('admin.home', 'Dashboard', [], 0, ['icon' => 'fa fa-dashboard']);
     $menu->dropdown('Articles', function ($sub) {
-        $sub->route('admin.articles.index', 'All Articles');
-        $sub->route('admin.articles.create', 'Add New');
-        $sub->divider();
-        $sub->route('admin.categories.index', 'Categories');
-    }, ['icon' => 'fa fa-book']);
+        $sub->route('admin.articles.index', 'All Articles', [], 1);
+        $sub->route('admin.articles.create', 'Add New', [], 2);
+        $sub->divider(3);
+        $sub->route('admin.categories.index', 'Categories', [], 4);
+    }, 1, ['icon' => 'fa fa-book']);
     $menu->dropdown('Pages', function ($sub) {
-        $sub->route('admin.pages.index', 'All Pages');
-        $sub->route('admin.pages.create', 'Add New');
-    }, ['icon' => 'fa fa-flag']);
+        $sub->route('admin.pages.index', 'All Pages', [], 1);
+        $sub->route('admin.pages.create', 'Add New', [], 2);
+    }, 2, ['icon' => 'fa fa-flag']);
     $menu->dropdown('Users', function ($sub) {
-        $sub->route('admin.users.index', 'All Users');
-        $sub->route('admin.users.create', 'Add New');
-        $sub->divider();
-        $sub->route('admin.roles.index', 'Roles');
-        $sub->route('admin.permissions.index', 'Permissions');
-    }, ['icon' => 'fa fa-users']);
+        $sub->route('admin.users.index', 'All Users', [], 1);
+        $sub->route('admin.users.create', 'Add New', [], 2);
+        $sub->divider(3);
+        $sub->route('admin.roles.index', 'Roles', [], 4);
+        $sub->route('admin.permissions.index', 'Permissions', [], 5);
+    }, 3, ['icon' => 'fa fa-users']);
 });
