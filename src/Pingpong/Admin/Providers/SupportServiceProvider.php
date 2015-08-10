8a69283c98a31b1d44b60ebcba28d37b7c0ebafd
Gravitano <gravitano16@gmail.com>
2015-08-11 00:51:49 +0700

    clean up filters

--- src/Pingpong/Admin/Providers/SupportServiceProvider.php
+++ src/Pingpong/Admin/Providers/SupportServiceProvider.php
@@ -32,7 +32,6 @@ class SupportServiceProvider extends ServiceProvider
     public function boot()
     {
         require __DIR__ . '/../permissions.php';
-        require __DIR__ . '/../filters.php';
         require __DIR__ . '/../composers.php';
         require __DIR__ . '/../helpers.php';
         require __DIR__ . '/../observers.php';
--- src/Pingpong/Admin/filters.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Pingpong\Admin\Filters\AuthFilter;
-use Pingpong\Admin\Filters\GuestFilter;
-
-Route::filter('admin.auth', AuthFilter::className());
-
-Route::filter('admin.guest', GuestFilter::className());
